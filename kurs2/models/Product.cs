0026553fb74dd2f506ec050e30fa0804bb41c925
kotzuken <you@example.com>
2026-06-04 21:47:13 +0300

    Упрощение модели для удобной загрузки из JSON файлов

--- kurs2/models/Product.cs
+++ kurs2/models/Product.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace kurs2.models
 {
-    public abstract class Product
+    public class Product
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+
         public string Name { get; set; } = string.Empty;
+
         public decimal Price { get; set; }
+
         public int StockQuantity { get; set; }
-        public abstract decimal GetTotalPrice();
+
+        // Нужно ли взвешивать товар
+        public bool IsWeighted { get; set; }
     }
 }
