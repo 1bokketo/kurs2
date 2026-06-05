691612e4bb79f58af314ce6f4071121847703c9c
kotzuken <you@example.com>
2026-06-05 12:12:56 +0300

    Редактирование класса "Receipt" и реализация загрузки чека в файл

---
+++ kurs2/Services/ReceiptService.cs
@@ -0,0 +1,56 @@
+﻿using kurs2.models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//генерация чека и сохранение его в файл
+namespace kurs2.Services
+{
+    public class ReceiptService
+    {
+        public Receipt CreateReceipt(
+            List<CartItem> items,
+            decimal totalAmount,
+            decimal cashback,
+            string paymentMethod)
+        {
+            Receipt receipt = new Receipt();
+
+            receipt.PurchaseDate = DateTime.Now;
+            receipt.PurchasedItems = items;
+            receipt.TotalAmount = totalAmount;
+            receipt.CashbackEarned = cashback;
+            receipt.PaymentMethod = paymentMethod;
+
+            return receipt;
+        }
+
+        public void SaveReceiptToFile(Receipt receipt)
+        {
+            if (!Directory.Exists("Receipts"))
+            {
+                Directory.CreateDirectory("Receipts");
+            }
+
+            string fileName =
+                "Receipts\\Receipt_" +
+                DateTime.Now.ToString("yyyyMMdd_HHmmss") +
+                ".txt";
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine("ЧЕК");
+                writer.WriteLine("-------------------");
+                writer.WriteLine("Дата: " + receipt.PurchaseDate);
+                writer.WriteLine("Оплата: " + receipt.PaymentMethod);
+                writer.WriteLine("Сумма: " + receipt.TotalAmount);
+                writer.WriteLine("Кэшбек: " + receipt.CashbackEarned);
+            }
+        }
+    }
+}
--- kurs2/models/Receipt.cs
+++ kurs2/models/Receipt.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+//добавлены новые методы для генерации и сохранения чека, а также свойства для хранения информации о покупке, таких как дата, список купленных товаров, общая сумма, кэшбек и способ оплаты.
 namespace kurs2.models
 {
     public class Receipt
     {
         public Guid Id { get; set; }
         public DateTime PurchaseDate { get; set; }
+        public List<CartItem> PurchasedItems { get; set; }
         public decimal TotalAmount { get; set; }
+        public decimal CashbackEarned { get; set; }
+        public string PaymentMethod { get; set; }
+        public Receipt()
+        {
+            Id = Guid.NewGuid();
+            PurchaseDate = DateTime.Now;
+            PurchasedItems = new List<CartItem>();
+            PaymentMethod = "";
+        }
     }
 }
