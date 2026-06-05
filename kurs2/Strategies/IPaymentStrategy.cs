b13a396c1f474106aef7b0598205faf0ca4e6df6
kotzuken <you@example.com>
2026-06-05 11:51:38 +0300

    создание метода оплаты

---
+++ kurs2/Strategies/IPaymentStrategy.cs
@@ -0,0 +1,16 @@
+﻿using kurs2.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//создание метода оплаты для каждого класса 
+namespace kurs2.Strategies
+{
+    public interface IPaymentStrategy
+    {
+        bool Pay(Customer customer, decimal amount);
+    }
+}
