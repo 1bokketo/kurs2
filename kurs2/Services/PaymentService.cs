48a99103475f606e0ee1ef7b21834cafc443d393
kotzuken <you@example.com>
2026-06-05 11:56:41 +0300

    создание метода оплаты

---
+++ kurs2/Services/PaymentService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kurs2.models;
+using kurs2.Strategies;
+
+
+
+namespace kurs2.Services
+{
+        public class PaymentService
+        {
+            public bool Pay(
+                Customer customer,
+                decimal amount,
+                IPaymentStrategy strategy)
+            {
+                return strategy.Pay(customer, amount);
+            }
+        }
+   }
+
